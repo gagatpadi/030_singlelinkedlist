cd47573c1bf27a28797308d174fa29d7416866f5
gagatpadi <padiyogaradhana@gmail.com>
2024-04-25 17:21:14 +0700

    void addnode

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -8,3 +8,18 @@ struct Node {
 };
 
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	
+
+}
